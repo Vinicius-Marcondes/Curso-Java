bce899c876cb0b159401b21b3d0d721b51533a9e
Vinicius-Marcondes <viniciusmarc_@hotmail.com>
2020-04-05 17:15:01 -0300

    Started OO

---
+++ src/curso/exercicios/oo/compose/car/Engine.java
@@ -0,0 +1,14 @@
+package curso.exercicios.oo.compose.car;
+
+public class Engine {
+    boolean turned = false;
+    double injection = 1;
+
+    int rotation(){
+        if(!turned){
+            return 0;
+        } else {
+            return (int) Math.round(injection * 3000);
+        }
+    }
+}
