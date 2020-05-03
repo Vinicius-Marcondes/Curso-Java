d676d1538f57d180e76ca77ca8ca6c54a0578744
Vinicius-Marcondes <viniciusmarc_@hotmail.com>
2020-05-03 01:56:51 -0300

    Some refactoring to 'game' exercise

--- src/curso/exercicios/oo/inheritance/firstExample/Hero.java
+++ src/curso/exercicios/oo/inheritance/firstExample/Hero.java
@@ -1,11 +1,9 @@
 package curso.exercicios.oo.inheritance.firstExample;
 
 public class Hero extends Player{
-    // private final int[] position = new int[2];
 
     Hero(int x, int y){
-        super.position[0] = x;
-        super.position[1] = y;
+        super(x, y);
     }
 
     boolean attack(Player opponent){
--- src/curso/exercicios/oo/inheritance/firstExample/Monster.java
+++ src/curso/exercicios/oo/inheritance/firstExample/Monster.java
@@ -1,8 +1,8 @@
 package curso.exercicios.oo.inheritance.firstExample;
 
 public class Monster extends Player{
+
     Monster(int x, int y){
-        super.position[0] = x;
-        super.position[1] = y;
+        super(x, y);
     }
 }
