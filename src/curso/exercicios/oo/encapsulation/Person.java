609f5bbfc5bde0f88cb26f390d7d03bceb785644
Vinicius-Marcondes <viniciusmarc_@hotmail.com>
2020-05-10 19:57:04 -0300

    Encapsulation exercise

--- src/curso/exercicios/oo/encapsulation/Person.java
+++ src/curso/exercicios/oo/encapsulation/Person.java
@@ -1,4 +1,13 @@
 package curso.exercicios.oo.encapsulation;
 
 public class Person {
+    public int age;
+
+    Person(int age){
+        this.age = age;
+    }
+
+    int getAge(){
+        return age;
+    }
 }
---
+++ src/curso/exercicios/oo/encapsulation/PersonTest.java
@@ -0,0 +1,9 @@
+package curso.exercicios.oo.encapsulation;
+
+public class PersonTest {
+    public static void main(String[] args) {
+
+        Person p1 = new Person();
+        p1.age = 19;
+    }
+}
